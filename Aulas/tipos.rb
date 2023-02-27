ee203f878ef3fd15a97ffc9cf78406bf1702c917
Maria Eduarda <crmariaeduarda2003@gmail.com>
2023-02-27 11:17:45 -0400

    primitive and complex types

---
+++ Aulas/tipos.rb
@@ -0,0 +1,13 @@
+# TIPOS PRIMITIVOS #
+# Tipos básicos da computação
+=begin
+    -> Inteiro - x = 1
+    -> Real/Float - y = 3.45
+    -> Caracteres/Strings - z = "abc"
+    -> lógico/Booleano - w = true 
+=end
+
+# Usamos os tipos primitivos para criar os tipos complexos
+
+# TIPOS COMPLEXOS #
+# Podemos dizer que os tipos complexos são as classes/objetos
