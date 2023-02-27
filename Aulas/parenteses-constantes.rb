46fd8fa41ced8c47814965384bb84a8ebbc5f59a
Maria Eduarda <crmariaeduarda2003@gmail.com>
2023-02-27 11:09:49 -0400

    parentheses and constants

---
+++ Aulas/parenteses-constantes.rb
@@ -0,0 +1,16 @@
+# PARÊNTESES #
+# No Ruby, o uso é opcional
+puts ("Cursos Rails")
+puts "Cursos Rails"
+
+puts "------------"
+
+# CONSTANTES #
+# São usadas para representar valores
+# Letras maiúsculas
+NOME = "Maria"
+PI = 3.16
+puts NOME
+puts PI
+
+# O Ruby diferencia letrais maiúsculas de minúsculas 
