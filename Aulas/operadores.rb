b4c1be5854de7314a7bea3fc27787163ecf07f88
Maria Eduarda <crmariaeduarda2003@gmail.com>
2023-02-27 07:59:43 -0400

    operators

---
+++ Aulas/operadores.rb
@@ -0,0 +1,31 @@
+# RELACIONAIS #
+# >, >=, <, <=, ==, !=
+=begin
+> - maior que
+>= - maior ou igual
+< - menor que
+<= - menor ou igual
+== - igual
+!= - diferente que
+=end
+if 1 < 2
+    puts 'a'
+else 
+    puts 'b'
+end
+
+# ARITÉTICOS #
+# +, -, *, /, **, %
+=begin
++ - soma
+- - subtração
+* - multiplicação
+/ - divisão
+** - potência
+% - módulo
+=end
+if 1 ** 2
+    puts 'a'
+else 
+    puts 'b'
+end
