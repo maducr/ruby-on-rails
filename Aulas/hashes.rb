b3c4af4b9c061b7952c193199dfd4ec792f98b49
Maria Eduarda <crmariaeduarda2003@gmail.com>
2023-02-27 10:04:46 -0400

    hashes

---
+++ Aulas/hashes.rb
@@ -0,0 +1,12 @@
+# HASH #
+# Uma lista do tipo CHAVE => VALOR
+
+# Forma tradicional:
+a = {"x" => 15, "curso" => "rails"}
+puts a["curso"]
+
+puts "------------"
+
+# Forma acima da versão 1.9:
+b = {"x": 15, "curso": "rails"}
+puts b["curso"]
