80b6d5b9aec076bcca55ebb1846edba04e8a0471
Maria Eduarda <crmariaeduarda2003@gmail.com>
2023-02-28 07:43:35 -0400

    class

--- Aulas/classes/pessoa.rb
+++ Aulas/classes/pessoa.rb
@@ -1,5 +1,18 @@
+=begin
 class Pessoa
     
 end
+=end 
 
 # A classe sempre começa com uma letra maiúscula (capitulada)
+# Nomes compostos devem capitular o início de cada palavra
+# o nome do arquivo não é obrigado ser o mesmo da classe
+
+# MÉTODOS - Aquilo que as classes podem fazer #
+# Use o def / end
+
+class Pessoa
+    def falar
+        "Olá, pessoal!"
+    end
+end
