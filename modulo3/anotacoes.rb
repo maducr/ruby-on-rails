08f192de30f534494e6aa534085bdcf56eeb9d87
Maria Eduarda <crmariaeduarda2003@gmail.com>
2023-03-02 12:39:44 -0400

    update

---
+++ modulo3/anotacoes.rb
@@ -0,0 +1,27 @@
+# RUBY #
+# Documentação - https://ww.ruby-lang.org/pt/documentation/
+
+# Core - http://ruby-doc.org/core-2.5.1/
+
+# Standard Library - http://ruby-doc.org/stdlib-2.5.1
+
+# API Dock - https://apidock.com/ruby
+
+# RAILS #
+# Guides - http://guides.rubyonrails.org/
+
+# API - http://api.rubyonrails.orgs/
+
+# API Dock - https://apidock.com/rails
+
+# Documentação Offline - http://devdocs.io/
+
+# COMO CRIAR UMA APLICAÇÃO #
+# rails new <nome da aplicação>
+
+# USANDO OUTRO BANCO DE DADOS #
+# Para saber qual o banco de dados que está sendo usado na aplicação, é preciso verificar o arquivo config/database.yml
+# Digitando rails fora da pasta podemos ver a opção --database que permite a configuração para outros bancos de dados - rails new meu_app --database=<nome bd>
+
+# PASTAS DA APLICAÇÃO #
+# Explicação da função de cada pasta - https://guides.rubyonrails.org/v5.2/getting_started.html#creating-the-blog-application
