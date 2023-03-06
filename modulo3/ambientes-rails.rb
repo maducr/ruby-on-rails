9da6212d2d1a1b43e153f9dae22c76908fae018e
Maria Eduarda <crmariaeduarda2003@gmail.com>
2023-03-06 08:14:20 -0400

    update

---
+++ modulo3/ambientes-rails.rb
@@ -0,0 +1,11 @@
+# AMBIENTES DO RAILS #
+
+# Por parão, o Rails vem com 3 ambientes: Desenvolvimento, Teste e Produção
+
+# Esses ambientes possibilitam ter configurações isoladas para cada ambiente
+
+# A Gemfile é a forma mais simplies de perceber isso
+
+# https://guides.rubyonrails.org/configuring.html#creating-rails-environments
+
+# Rodar o ambiente de produção: RAILS_ENV=production rails s ou rails s -e production
---
+++ modulo3/secaffold.rb
@@ -0,0 +1,7 @@
+# rails generate scaffold < Model> < campo:tipo> < campo:tipo>
+
+# TIPOS DOS CAMPOS:
+# - https://api.rubyrails.org/classes/ActiveRecord/ConnectionAdapters/SchemaStatements.html#method-i-add_column
+# - description:string
+# - acronym:string
+# - url_image:string
